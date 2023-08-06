67928d9eaadc37e5f0ff01189eef43cfedd1a1c5
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-08-06 17:04:21 -0400

    chore(rename): spotCheck -> check

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -2,8 +2,8 @@ import category from './documents/category'
 import checkout from './documents/checkout'
 import item from './documents/item'
 import kit from './documents/kit'
-import spotCheck from './documents/spotCheck'
+import check from './documents/check'
 import tag from './documents/tag'
 import user from './documents/user'
 
-export const schema = [item, category, tag, kit, checkout, user, spotCheck]
+export const schema = [item, category, tag, kit, checkout, user, check]
