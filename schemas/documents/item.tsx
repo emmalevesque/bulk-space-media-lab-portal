da2080993753651a5c995a568d55ad3dd0bfa367
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-08-06 17:02:26 -0400

    chore: deprecate inventoryItem, merge with item

--- schemas/documents/item.tsx
+++ schemas/documents/item.tsx
@@ -89,22 +89,66 @@ export const ItemPreviewPane = ({ document }) => {
 }
 
 export default defineType({
-  name: 'item',
+  name: 'inventoryItem',
   title: 'Inventory Item',
   type: 'document',
+  groups: [
+    {
+      title: 'Details',
+      name: 'details',
+    },
+    {
+      title: 'Item Images',
+      name: 'images',
+    },
+    {
+      title: 'Taxonomy',
+      name: 'taxonomy',
+    },
+    {
+      title: 'Item Stock',
+      name: 'stock',
+    },
+    {
+      title: 'Cost and Condition',
+      name: 'condition',
+    },
+  ],
   fields: [
     {
-      name: 'name',
-      title: 'Item Name',
+      name: 'easyName',
+      title: 'Easy Name',
+      description:
+        'Commonly used name for the item e.g. DSLR Camera, Cord, Adapter...',
       type: 'string',
+      group: 'details',
       validation: (Rule) => Rule.required(),
     },
     {
+      group: 'details',
+      name: 'manufacturerDetails',
+      title: 'Manufacturer Details',
+      type: 'object',
+      fields: [
+        {
+          name: 'manufacturer',
+          title: 'Manufacturer',
+          type: 'string',
+        },
+        {
+          name: 'model',
+          title: 'Model',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      group: 'details',
       name: 'slug',
       title: 'Item Slug',
       type: 'slug',
       options: {
-        source: 'name',
+        source: 'easyName',
         maxLength: 96,
       },
       components: {
@@ -112,45 +156,49 @@ export default defineType({
       },
     },
     {
+      group: 'details',
       name: 'sku',
       title: 'Item SKU',
       type: 'string',
       validation: (Rule) => Rule.required(),
     },
     {
+      group: 'details',
       name: 'description',
       title: 'Item Description',
       type: 'text',
       validation: (Rule) => Rule.required(),
     },
     {
+      group: 'condition',
       name: 'cost',
       title: 'Item Price',
       type: 'number',
     },
     {
+      group: 'images',
       name: 'images',
-      title: 'Item Image',
+      title: 'Item Images',
       type: 'array',
       of: [{ type: 'image' }],
     },
     {
+      group: 'taxonomy',
       name: 'category',
       title: 'Item Category',
       type: 'reference',
-
       to: [{ type: 'category' }],
       validation: (Rule) => Rule.required(),
     },
     {
+      group: 'taxonomy',
       name: 'tags',
-      title: 'Item Tags',
-
       type: 'array',
-      of: [{ type: 'string' }],
-      validation: (Rule) => Rule.required(),
+      of: [{ type: 'reference', to: [{ type: 'tag' }] }],
+      title: 'Item Tags',
     },
     {
+      group: 'condition',
       name: 'stock',
       title: 'Item Stock',
       type: 'number',
@@ -161,12 +209,22 @@ export default defineType({
       type: 'boolean',
       validation: (Rule) => Rule.required(),
     },
-    // {
-    //   name: 'brand',
-    //   title: 'Item Brand',
-    //   type: 'reference',
-    //   to: [{ type: 'brand' }],
-    //   validation: (Rule) => Rule.required(),
-    // },
+    {
+      group: 'details',
+      name: 'productManualUrl',
+      title: 'Product Manual URL',
+      description:
+        'Product Manual URL (if applicable) OR revelvant and helpful links',
+      type: 'url',
+    },
+    {
+      group: 'condition',
+      name: 'condition',
+      title: 'Condition',
+      type: 'string',
+      options: {
+        list: ['New', 'Good', 'Fair', 'Poor'],
+      },
+    },
   ],
 })
--- schemas/schema.ts
+++ schemas/schema.ts
@@ -1,19 +1,9 @@
 import category from './documents/category'
 import checkout from './documents/checkout'
-import inventoryItem from './documents/inventoryItem'
 import item from './documents/item'
 import kit from './documents/kit'
 import spotCheck from './documents/spotCheck'
 import tag from './documents/tag'
 import user from './documents/user'
 
-export const schema = [
-  item,
-  inventoryItem,
-  category,
-  tag,
-  kit,
-  checkout,
-  user,
-  spotCheck,
-]
+export const schema = [item, category, tag, kit, checkout, user, spotCheck]
