356482b2c3523acc5cf417887b8b8e826dbffcad
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-08-12 15:04:32 -0400

    feat: temporary redirect from root url

--- app/(portal)/layout.tsx
+++ app/(portal)/layout.tsx
@@ -1,3 +1,5 @@
+import "tailwindcss/tailwind.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -5,5 +7,5 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
