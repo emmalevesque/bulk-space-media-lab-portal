12bf26d4bc5cb7bd668f6e72f966681dacf3a106
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-10-06 18:50:45 -0400

    fix: route handler

--- app/(api)/api/algolia/sync/route.ts
+++ app/(api)/api/algolia/sync/route.ts
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
 import { client } from 'lib/sanity.client'
 import { sanityAlgolia } from '../_algolia'
+import { NextRequest, NextResponse } from 'next/server'
 
 const sanity = client
 
@@ -8,22 +8,24 @@ const sanity = client
  *  This function receives webhook POSTs from Sanity and updates, creates or
  *  deletes records in the corresponding Algolia indices.
  */
-export async function POST(req: VercelRequest, res: VercelResponse) {
-  // Tip: Add webhook secrets to verify that the request is coming from Sanity.
-  // See more at: https://www.sanity.io/docs/webhooks#bfa1758643b3
-  if (req.headers['content-type'] !== 'application/json') {
-    res.status(400)
-    res.json({ message: 'Bad request' })
-    return
+export async function POST(req: NextRequest, res: NextResponse) {
+  const requestHeaders = new Headers(req.headers)
+  if (requestHeaders.get('content-type') !== 'application/json') {
+    return Response.json({
+      status: 400,
+      message: 'Bad request',
+    })
   }
+
+  const body = req.json()
 
   // Finally connect the Sanity webhook payload to Algolia indices via the
   // configured serializers and optional visibility function. `webhookSync` will
   // inspect the webhook payload, make queries back to Sanity with the `sanity`
   // client and make sure the algolia indices are synced to match.
   return sanityAlgolia
-    .webhookSync(sanity, req.body)
-    .then(() => res.status(200).send('ok'))
+    .webhookSync(sanity, await body)
+    .then(() => Response.json({ status: 200, message: 'OK' }))
     .catch((err) => {
       return {
         statusCode: 500,
