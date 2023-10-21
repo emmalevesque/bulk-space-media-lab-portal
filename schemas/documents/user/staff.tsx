f2f7fd107a5355c2617fed99b6555156f5c4e865
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-10-21 17:25:03 -0400

    feat: use user fields for staff

--- schemas/documents/user/staff.tsx
+++ schemas/documents/user/staff.tsx
@@ -1,48 +1,10 @@
-import EmojiIcon from 'components/Icon/Emoji'
 import { defineType } from 'sanity'
+import user from './user'
+import EmojiIcon from 'components/Icon/Emoji'
 
 export default defineType({
+  ...user,
   name: 'staff',
   title: 'Staff',
-  type: 'document',
   icon: () => <EmojiIcon>🫂</EmojiIcon>,
-  groups: [
-    {
-      name: 'contact',
-      default: true,
-    },
-    {
-      name: 'photo',
-    },
-  ],
-  fields: [
-    {
-      group: 'contact',
-      name: 'contact',
-      title: 'Contact Information',
-      type: 'contact',
-    },
-    {
-      name: 'photo',
-      title: 'Photo',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-  ],
-  preview: {
-    select: {
-      contact: 'contact',
-      subtitle: 'contactInformation.email',
-      media: 'photo',
-    },
-    prepare({ contact, subtitle, media }) {
-      return {
-        title: `${contact?.name?.first} ${contact?.name?.last}`,
-        subtitle,
-        media,
-      }
-    },
-  },
 })
