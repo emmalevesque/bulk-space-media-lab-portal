de6508ec23103e44ff0c005b234e7308543c8363
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-10-23 18:25:05 -0400

    fix: QR Code

--- schemas/documents/inventory/components/User/UserQrCodePane.tsx
+++ schemas/documents/inventory/components/User/UserQrCodePane.tsx
@@ -9,7 +9,7 @@ export default (props) => {
 
   const document = props?.document?.displayed
 
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}${basePath}/manageInventory;inventoryItems;allItems;c325264f-c613-43d4-a40d-2d5ffa2777c2`
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}${basePath}/manageInventory;inventoryItems;allItems;${document._id}`
 
   return document ? (
     <Box padding={4}>
