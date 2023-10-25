f66cc32125d996c94ef3e7efe987933c6b331a97
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-10-25 10:31:16 -0400

    feat: add toolbar to studio component

--- app/(studio)/studio/[[...index]]/Studio.tsx
+++ app/(studio)/studio/[[...index]]/Studio.tsx
@@ -1,10 +1,19 @@
 'use client'
 
+import { StaffToolbar } from 'components/StaffToolbar'
 import { NextStudio } from 'next-sanity/studio'
+import { Suspense } from 'react'
 
 import config from 'sanity.config'
 
 export function Studio() {
   //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
-  return <NextStudio config={config} />
+  return (
+    <>
+      <NextStudio config={config} />
+      <Suspense>
+        <StaffToolbar />
+      </Suspense>
+    </>
+  )
 }
