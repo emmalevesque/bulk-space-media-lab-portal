88e8f7ecf28ad428cd995c9245424af725da3446
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2023-12-08 19:45:16 -0500

    feat: incorporate recursion

--- plugins/navigationStructure.tsx
+++ plugins/navigationStructure.tsx
@@ -6,7 +6,7 @@ import { StructureBuilder } from 'sanity/desk'
 
 const typeIconMap = {
   category: () => <EmojiIcon>🏷️</EmojiIcon>,
-  product: () => <EmojiIcon>🎒</EmojiIcon>,
+  item: () => <EmojiIcon>🎒</EmojiIcon>,
   tag: () => <EmojiIcon>🏷️</EmojiIcon>,
 }
 
@@ -62,31 +62,46 @@ export default function navigationStructure(
 
   // create the recursive structure builder
   const recursiveStructureBuilder = (item: any) => {
-    return S.listItem()
-      .title(item.name)
-      .id(item._id)
-      .icon(typeIconMap[item._type])
-      .child(
-        S.list()
-          .title(item.name)
-          .id(item._id)
-          .items([
-            S.listItem()
-              .title(`Edit ${item.name}`)
-              .id(`${item._id}-edit`)
-              .icon(() => <EmojiIcon>📝</EmojiIcon>)
-              .child(
-                S.document()
-                  .title(`Edit ${item.name}`)
-                  .id(item._id)
-                  .schemaType(item._type)
-              ),
-            S.divider(),
-            ...item.children.map((child) => {
-              return recursiveStructureBuilder(child)
-            }),
-          ])
-      )
+    console.log({ item })
+
+    if (item._type === 'category') {
+      return S.listItem()
+        .title(item.name)
+        .id(item._id)
+        .icon(() => <EmojiIcon>🏷️</EmojiIcon>)
+        .child(
+          S.list()
+            .title(item.name)
+            .id(item._id)
+            .items([
+              S.listItem()
+                .title(`Edit ${item.name}`)
+                .id(`${item._id}-edit`)
+                .icon(() => <EmojiIcon>📝</EmojiIcon>)
+                .child(
+                  S.document()
+                    .title(`Edit ${item.name}`)
+                    .id(item._id)
+                    .schemaType(item._type)
+                ),
+              S.divider(),
+              ...item.children.map((child) => {
+                return recursiveStructureBuilder(child)
+              }),
+            ])
+        )
+    } else {
+      return S.listItem()
+        .title(item.name)
+        .id(item._id)
+        .icon(typeIconMap[item._type])
+        .child(
+          S.document()
+            .title(`Edit ${item.name}`)
+            .id(item._id)
+            .schemaType(item._type)
+        )
+    }
   }
 
   return S.listItem()
@@ -96,94 +111,16 @@ export default function navigationStructure(
     .child(
       documentStore.listenQuery(query, {}, options).pipe(
         map((response: any) => {
-          // create separate lists for items and categories
-
-          const categories = response.filter(
-            (item) => item._type === 'category'
-          )
+          console.log({ response })
 
           return S.list()
             .title('Manage Inventory')
-            .items(
-              categories.map((item) => {
-                return S.listItem()
-                  .title(item.name)
-                  .id(item._id)
-                  .icon(() => <EmojiIcon>🏷️</EmojiIcon>)
-                  .child(
-                    S.list()
-                      .title(item.name)
-                      .id(item._id)
-                      .items([
-                        S.listItem()
-                          .title(`Edit ${item.name}`)
-                          .id(`${item._id}-edit`)
-                          .icon(() => <EmojiIcon>📝</EmojiIcon>)
-                          .child(
-                            S.document()
-                              .title(`Edit ${item.name}`)
-                              .id(item._id)
-                              .schemaType(item._type)
-                          ),
-                        S.divider(),
-                        ...item.children.map((child) => {
-                          if (child._type === 'category') {
-                            return S.listItem()
-                              .title(child.name)
-                              .id(child._id)
-                              .icon(() => <EmojiIcon>🏷️</EmojiIcon>)
-                              .child(
-                                S.list()
-                                  .title(child.name)
-                                  .id(child._id)
-                                  .items(
-                                    child.children.map((subChild) => {
-                                      return S.listItem()
-                                        .title(subChild.name)
-                                        .id(subChild._id)
-                                        .icon(typeIconMap[subChild._type])
-                                        .child(
-                                          S.list()
-                                            .title(subChild.name)
-                                            .id(subChild._id)
-                                            .items(
-                                              subChild.children?.map(
-                                                (subSubChild) => {
-                                                  console.log({ subChild })
-
-                                                  return S.listItem()
-                                                    .title(subSubChild.name)
-                                                    .id(subSubChild._id)
-                                                    .icon(
-                                                      typeIconMap[
-                                                        subSubChild._type
-                                                      ]
-                                                    )
-                                                    .child(
-                                                      S.document()
-                                                        .id(subSubChild._id)
-                                                        .schemaType(
-                                                          subSubChild._type
-                                                        )
-                                                    )
-                                                }
-                                              )
-                                            )
-                                        )
-                                    })
-                                  )
-                              )
-                          } else {
-                            return S.listItem()
-                              .title(child.name)
-                              .id(child._id)
-                              .child(S.list().title(child.name).id(child._id))
-                          }
-                        }),
-                      ])
-                  )
-              })
-            )
+            .id('manage-inventory')
+            .items([
+              ...response.map((item: any) => {
+                return recursiveStructureBuilder(item)
+              }),
+            ])
         })
       )
     )
