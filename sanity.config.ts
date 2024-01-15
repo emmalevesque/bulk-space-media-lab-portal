f65ea5fb69a52504cf49055b596520f5bf86035a
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2024-01-15 11:31:03 -0500

    feat: add role based logic for tools

--- sanity.config.ts
+++ sanity.config.ts
@@ -52,7 +52,25 @@ const document = {
       : prev,
 }
 
-const tools = [inventoryStatsComponent()]
+const tools = (prev, context) => {
+  const canManageEmbeddingsIndex = context.currentUser?.roles
+    .map((role) => role.name)
+    .some((roleName) => ['administrator', 'developer'].includes(roleName))
+
+  const canViewInventoryStats = context.currentUser?.roles
+    .map((role) => role.name)
+    .some((roleName) => ['administrator', 'developer'].includes(roleName))
+
+  const availableTools = [...prev, inventoryStatsComponent()]
+
+  return !canViewInventoryStats && !canManageEmbeddingsIndex
+    ? [...prev.filter((tool) => tool.name !== 'embeddings-index')]
+    : !canViewInventoryStats && canManageEmbeddingsIndex
+    ? [...availableTools.filter((tool) => tool.name !== 'inventory-stats')]
+    : !canManageEmbeddingsIndex && canViewInventoryStats
+    ? [...availableTools.filter((tool) => tool.name !== 'embeddings-index')]
+    : [...availableTools]
+}
 
 const plugins = [
   embeddingsIndexReferenceInput(),
