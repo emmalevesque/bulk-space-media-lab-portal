9e6b7ca3950b2898bf3853fa995aef5d4e89e2ef
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2024-04-10 14:44:32 -0400

    fix: loading fallback cateogry input container

--- components/global/LoadingOverlay.tsx
+++ components/global/LoadingOverlay.tsx
@@ -2,11 +2,9 @@ import { Flex, Spinner } from '@sanity/ui'
 
 export default function LoadingOverlay() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-white">
-      <Flex direction={'column'} align={'center'} gap={3}>
-        Loading
-        <Spinner muted />
-      </Flex>
-    </div>
+    <Flex direction={'column'} align={'center'} gap={3}>
+      Loading
+      <Spinner muted />
+    </Flex>
   )
 }
