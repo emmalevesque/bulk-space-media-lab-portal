e1945265f21234820d1d543dab5a6931b175091e
emma levesque-schaefer <emma.levesque.schaefer@gmail.com>
2024-06-04 18:13:15 -0400

    feat: add color tag to item

--- schemas/documents/colorTag.tsx
+++ schemas/documents/colorTag.tsx
@@ -34,7 +34,7 @@ export default defineType({
               style={{
                 width: '100%',
                 height: '100%',
-                backgroundColor: color.value,
+                backgroundColor: color?.value || '',
               }}
             ></div>
           )
